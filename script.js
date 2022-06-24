1b99ea62e4f7f402cc133b4e2c9355ca0f6a362a
OluwatobilobaAbass <105176168+OluwatobilobaAbass@users.noreply.github.com>
2022-06-24 04:08:53 +0100

    name on console

---
+++ script.js
@@ -0,0 +1,9 @@
+myName = 'Oluwatobi Abass';
+myHeight = '5ft4inches';
+myCountry = 'Nigeria';
+
+console.log('Name: '+ myName);
+console.log('Height: ' + myHeight);
+console.log('Country: ' + myCountry);
+
+//view in console
